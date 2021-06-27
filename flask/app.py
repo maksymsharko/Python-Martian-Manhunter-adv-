9a18e1c43979d973eb6cf9a0ec867901e3dbcc39
Maksym Sharko <maks.sharko02@gmail.com>
2021-06-27 11:49:18 +0300

    Adding hw12

--- flask/app.py
+++ flask/app.py
@@ -1,24 +1,79 @@
 from flask import Flask, render_template, request, jsonify, Response
+from flask_restful import Resource, Api
 import requests
 from config import Config
-
+import json
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"])
+@app.route('/', methods=['GET'])
 def homepage():
     return render_template("homepage.html")
 
 
-@app.route("/search", methods=["POST"])
+@app.route('/search', methods=['POST'])
 def search_weather():
-    weather = []
-    cities = request.form.get("cities")
-    cities = cities.replace(" ", "")
-    cities_list = cities.split(",")
-    for city in cities_list:
-        querystring = {"q": city, "cnt": "1", "mode": "null", "lon": "0", "type": "link, accurate", "lat": "0",
+    city = request.form.get("city")
+    querystring = {"q": city, "cnt": "1", "mode": "null", "lon": "0", "type": "link, accurate", "lat": "0",
+                   "units": "metric"}
+
+    headers = {
+        'x-rapidapi-key': Config.WEATHER_API_KEY,
+        'x-rapidapi-host': Config.WEATHER_API_HOST
+    }
+
+    response = requests.request("GET", Config.WEATHER_API_URL, headers=headers, params=querystring)
+    if response.status_code == 200:
+        data = response.json()
+        weather = data['list'][0]
+        return render_template("weather.html", weather=weather)
+
+    return Response(status=404)
+
+
+api = Api(app)
+
+todos = {}
+
+
+class Todo(Resource):
+
+    def get(self, todo_id):
+        try:
+            data = {todo_id: todos[todo_id]}
+        except KeyError:
+            return Response("Not found", status=404)
+        return data
+
+    def put(self, todo_id):
+        todos[todo_id] = request.json.get('text')
+        data = {todo_id: todos[todo_id]}
+        with open('todos.json', 'w') as write_file:
+            json.dump(data, write_file)
+        write_file.close()
+        return data
+
+    def delete(self, todo_id):
+        del todos[todo_id]
+        return Response(todos, status=204)
+
+
+class TodoList(Resource):
+
+    def get(self):
+        return todos
+
+    def post(self):
+        todos[request.json.get('todo_id', None)] = request.json.get('text', "")
+        return todos
+
+
+class Weather(Resource):
+    def get(self):
+
+        city = request.form.get("city")
+        querystring = {"q": "city", "cnt": "1", "mode": "null", "lon": "0", "type": "link, accurate", "lat": "0",
                        "units": "metric"}
 
         headers = {
@@ -29,34 +84,15 @@ def search_weather():
         response = requests.request("GET", Config.WEATHER_API_URL, headers=headers, params=querystring)
         if response.status_code == 200:
             data = response.json()
-            try:
-                weather.append(data['list'][0])
-            except IndexError:
-                return Response(status=404)
-    return render_template("weather.html", weather=weather)
+            weather = data['list'][0]
+            return render_template("weather.html", weather=weather)
 
-
-@app.route("/search_coordinates", methods=["POST"])
-def search_weather_loc():
-    weather = []
-    lat = request.form.get("lat")
-    lon = request.form.get("lon")
-    querystring = {"q": "", "cnt": "1", "mode": "null", "lon": lon, "type": "link, accurate", "lat": lat,
-                   "units": "metric"}
-
-    headers = {
-        'x-rapidapi-key': Config.WEATHER_API_KEY,
-        'x-rapidapi-host': Config.WEATHER_API_HOST
-    }
-
-    response = requests.request("GET", Config.WEATHER_API_URL, headers=headers, params=querystring)
-    data = response.json()
-    if response.status_code == 200:
-        weather.append(data['list'][0])
-        return render_template("weather.html", weather=weather)
-    else:
         return Response(status=404)
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+api.add_resource(Weather, '/weather')
+api.add_resource(Todo, '/todos/<int:todo_id>')
+api.add_resource(TodoList, '/todos')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
