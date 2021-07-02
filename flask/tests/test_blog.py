aadb44ecf6ca3f65d334e3f03804273c9dfbb070
Максим <79326174+maksymsharko@users.noreply.github.com>
2021-07-02 15:05:38 +0300

    Update test_blog.py

--- flask/tests/test_blog.py
+++ flask/tests/test_blog.py
@@ -2,7 +2,7 @@ from tests.conftest import client
 
 
 def test_menu_items(client):
-    response = client.get('/menu_items')
-    data = response.json()
+    response = client.get("/menu-items")
+    data = response.json
     assert response.status_code == 200
-    assert data['items']['name'] == 'Articles'
+    assert data["items"][0]["name"] == "Articles"
