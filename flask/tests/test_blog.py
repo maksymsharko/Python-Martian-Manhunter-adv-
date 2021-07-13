72bad366394ca21894defbe6a8c217be35bc628a
Максим <79326174+maksymsharko@users.noreply.github.com>
2021-07-13 20:09:53 +0300

    Delete flask directory

--- flask/tests/test_blog.py
+++
@@ -1,8 +0,0 @@
-from tests.conftest import client
-
-
-def test_menu_items(client):
-    response = client.get('/menu_items')
-    data = response.json()
-    assert response.status_code == 200
-    assert data['items']['name'] == 'Articles'
