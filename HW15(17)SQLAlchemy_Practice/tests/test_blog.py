fe3a170929a4c9f004bcf6c68df994d4cb2df254
Maksym Sharko <maks.sharko02@gmail.com>
2021-07-15 20:17:18 +0300

    Revert "Adding HW15(17)"
    This reverts commit 1b0fc0fa

--- HW15(17)SQLAlchemy_Practice/tests/test_blog.py
+++
@@ -1,8 +0,0 @@
-from tests.conftest import client
-
-
-def test_menu_items(client):
-    response = client.get("/menu-items")
-    data = response.json
-    assert response.status_code == 404
-    assert data["items"][0]["name"] == "Articles"
